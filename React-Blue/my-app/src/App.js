fd2ff13fa84f5dfdb839d12b6c2dd6dc13740baa
Matheus Rodrigues <matheusrodriguesoliveira273@gmail.com>
2021-07-20 10:20:09 -0300

    update React

--- React-Blue/my-app/src/App.js
+++ React-Blue/my-app/src/App.js
@@ -3,25 +3,31 @@ import React from 'react';
 import './App.css';
 import github from './img/github.png'
 import linkedin from './img/linkedin.png'
+import fotoMatheus from './img/matheus.jpg'
+import fotoMolina from './img/gustavo-molina.jpeg'
+import fotoCervera from './img/gustavo-cervera.jpeg'
 
 const Equipe = (props) =>{
   return (
     <div>
-      <Sobre nome={props.nome} cargo={props.cargo} idade={props.idade} github={props.github} linkedin={props.linkedin}/>
+      <Sobre foto={props.foto} nome={props.nome} cargo={props.cargo} idade={props.idade} github={props.github} linkedin={props.linkedin}/>
       <hr/>
     </div>
   );
 }
 const Sobre = (props) =>{
   return(
-    <div className='info'>
+    <div className='container'>
       <h2>{props.nome}</h2>
+      <div className='info'>
+      <img className="foto-perfil" src={props.foto} width="100" alt="foto-perfil"></img>
       <h3>{props.cargo}</h3>
       <h3>{props.idade} anos</h3>
       <h3>
-        <a href={props.github} target="_blank"> <img src={github} width="25"></img></a>
-        <a href={props.linkedin} target="_blank"> <img src={linkedin} width="25"></img></a>
+        <a href={props.github} target="_blank" rel="noopener noreferrer"> <img src={github} width="25" alt="github"></img></a>
+        <a href={props.linkedin} target="_blank" rel="noopener noreferrer"> <img src={linkedin} width="25" alt="linkedin"></img></a>
       </h3>
+      </div>
     </div>
   );
 }
@@ -32,8 +38,9 @@ function App() {
       <header>
         <h1>Conheça nossa Equipe</h1>
       </header>
-      <Equipe nome="Matheus Rodrigues de Oliveira" cargo="Master Bluemer" idade="21" github="https://github.com/TheusmaOliver" linkedin="https://www.linkedin.com/in/theusmaoliver/" />
-      <Equipe nome="Gustavo Molina" cargo="Professor" idade="30" github="https://github.com/gustavomolina17" linkedin="https://www.linkedin.com/in/gustavo-molina-a2798418/" />
+      <Equipe foto={fotoMatheus} nome="Matheus Rodrigues de Oliveira" cargo="Master Bluemer" idade="21" github="https://github.com/TheusmaOliver" linkedin="https://www.linkedin.com/in/theusmaoliver/" />
+      <Equipe foto={fotoMolina} nome="Gustavo Molina" cargo="Professor" idade="30" github="https://github.com/gustavomolina17" linkedin="https://www.linkedin.com/in/gustavo-molina-a2798418/" />
+      <Equipe foto={fotoCervera} nome="Gustavo Cervera" cargo="Representante" idade="26" github="https://github.com/gucervus" linkedin="https://www.linkedin.com/in/gustavo-carvera/" />
     </div>
   );
 }
